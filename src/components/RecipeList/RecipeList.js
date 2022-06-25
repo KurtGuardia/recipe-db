b4890146a1698d62e5c6ded6cd258b324f318842
Kurt Guardia <kurtguardia@gmail.com>
2022-06-25 13:16:23 +0200

    Individual recipe

--- src/components/RecipeList/RecipeList.js
+++ src/components/RecipeList/RecipeList.js
@@ -5,13 +5,15 @@ import './RecipeList.css'
 
 export default function RecipeList({ recipes }) {
   return (
-    <div className="recipe-list">
-      {recipes.map(recipe => (
-        <div key={recipe.id} className="card">
+    <div className='recipe-list'>
+      {recipes.map((recipe) => (
+        <div key={recipe.id} className='card'>
           <h3>{recipe.title}</h3>
           <p>{recipe.cookingTime} to make.</p>
           <div>{recipe.method.substring(0, 100)}...</div>
-          <Link to={`/recipes/${recipe.id}`}>Cook This</Link>
+          <Link to={`/recipes/${recipe.id}`}>
+            Cook This
+          </Link>
         </div>
       ))}
     </div>
