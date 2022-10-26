7bacff7e5ef73523618758712cce28569b63292e
Kurt Guardia <kurtguardia@gmail.com>
2022-10-26 18:48:37 +0200

    Create form done

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,10 +1,93 @@
+import { useState, useRef } from 'react'
 // styles
 import './Create.css'
 
 export default function Create() {
+  const [title, setTitle] = useState('')
+  const [method, setMethod] = useState('')
+  const [cookingTime, setCookingTime] = useState('')
+  const [newIngredient, setNewIngredient] = useState('')
+  const [ingredients, setIngredients] = useState([])
+  const ingredientInput = useRef(null)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(title, method, cookingTime, ingredients)
+  }
+
+  const handleAdd = (e) => {
+    e.preventDefault()
+    const ing = newIngredient.trim()
+
+    if (ing && !ingredients.includes(ing)) {
+      setIngredients((preveIngredients) => [
+        ...preveIngredients,
+        ing,
+      ])
+    }
+    setNewIngredient('')
+    ingredientInput.current.focus()
+  }
+
   return (
-    <div>
-      Create
+    <div className='create'>
+      <h2 className='page-title'>Add a new Recipe</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Recipe title:</span>
+
+          <input
+            type='text'
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+            required
+          />
+        </label>
+
+        <label>
+          <span>Recipe Ingredients:</span>
+          <div className='ingredients'>
+            <input
+              type='text'
+              onChange={(e) =>
+                setNewIngredient(e.target.value)
+              }
+              value={newIngredient}
+              ref={ingredientInput}
+            />
+            <button className='btn' onClick={handleAdd}>
+              add
+            </button>
+          </div>
+        </label>
+        <p>
+          Current ingredietns:{' '}
+          {ingredients.map((i) => (
+            <em key={i}>{i}, </em>
+          ))}
+        </p>
+
+        <label>
+          <span>Recipe method:</span>
+          <textarea
+            onChange={(e) => setMethod(e.target.value)}
+            value={method}
+            required
+          />
+        </label>
+
+        <label>
+          <span>Cooking time (minutes)</span>
+          <input
+            type='number'
+            onChange={(e) => setCookingTime(e.target.value)}
+            value={cookingTime}
+            required
+          />
+        </label>
+
+        <button className='btn'>submit</button>
+      </form>
     </div>
   )
 }
